6f0a968278208041529c757308145c5156daddc6
Ridhwan Ahsan <135047921+ridhwanahsan@users.noreply.github.com>
2024-06-23 23:30:44 +0600

    erer

--- code.js
+++ code.js
@@ -1,3 +1,51 @@
 for (let i = 0; i < 10; ) {
     console.log(i);
 }
+for (let i = 0; i < 10; ) {
+    console.log(i);
+}
+for (let i = 0; i < 10; ) {
+    console.log(i);
+}
+for (let i = 0; i < 10; ) {
+    console.log(i);
+}
+for (let i = 0; i < 10; ) {
+    console.log(i);
+}
+for (let i = 0; i < 10; ) {
+    console.log(i);
+}
+for (let i = 0; i < 10; ) {
+    console.log(i);
+}
+for (let i = 0; i < 10; ) {
+    console.log(i);
+}
+for (let i = 0; i < 10; ) {
+    console.log(i);
+}
+for (let i = 0; i < 10; ) {
+    console.log(i);
+}
+for (let i = 0; i < 10; ) {
+    console.log(i);
+}
+for (let i = 0; i < 10; ) {
+    console.log(i);
+}
+for (let i = 0; i < 10; ) {
+    console.log(i);
+}
+for (let i = 0; i < 10; ) {
+    console.log(i);
+}
+for (let i = 0; i < 10; ) {
+    console.log(i);
+}
+for (let i = 0; i < 10; ) {
+    console.log(i);
+}
+for (let i = 0; i < 10; ) {
+    console.log(i);
+}
